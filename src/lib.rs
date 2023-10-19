c01bf781c85b38d1ab9df5e955ef6464d85729af
Chad Bro <ayechan.sant@gmail.com>
2023-10-18 17:13:21 -0700

    making deposits module

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,14 @@ mod erc721;
 mod helpers;
 mod pb;
 
-// use pb::schema::{Approval, Approvals, Transfer, Transfers};
+use pb::schema::{Transfer, Transfers, Deposit, Deposits, Withdraw, Withdraws};
 use substreams::pb::substreams::Clock;
 use substreams_entity_change::{pb::entity::EntityChanges, tables::Tables};
-// use substreams_ethereum::{pb::eth, Event};
+use substreams_ethereum::{pb::eth, Event};
 
 use helpers::*;
 
-// use erc721::events::{Approval as ApprovalEvent, Transfer as TransferEvent};
+use erc721::events::{Approval as ApprovalEvent, Transfer as TransferEvent};
 
 pub const ADDRESS: &str = "0xd90e2f925DA726b50C4Ed8D0Fb90Ad053324F31b";
 const START_BLOCK: u64 = 12287507;
@@ -23,7 +23,7 @@ fn map_transfers(block: eth::v2::Block) -> Result<Transfers, substreams::errors:
         .filter_map(|log| {
             if format_hex(log.address()) == ADDRESS.to_lowercase() {
                 if let Some(transfer) = TransferEvent::match_and_decode(log) {
-                    Some((transfer, format_hex(&log.receipt.transaction.hash)))
+                    Some((transfer, format_hex(&log.receipt.transaction.value)))
                 } else {
                     None
                 }
@@ -41,6 +41,32 @@ fn map_transfers(block: eth::v2::Block) -> Result<Transfers, substreams::errors:
 
     Ok(Transfers { transfers })
 }
+#[substreams::handlers::map]
+fn map_deposits(block: eth::v2::Block) -> Result<Desposits, substreams::errors::Error> {
+    let transfers = block
+        .logs()
+        .filter_map(|log| {
+            if format_hex(log.address()) == ADDRESS.to_lowercase() {
+                if let Some(transfer) = TransferEvent::match_and_decode(log) {
+                    Some((transfer, format_hex(&log.receipt.transaction.value)))
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .map(|(transfer, hash)| Transfer {
+            from: format_hex(&transfer.from),
+            to: format_hex(&transfer.to),
+            token_id: transfer.token_id.to_string(),
+            tx_hash: hash,
+        })
+        .collect::<Vec<Transfer>>();
+
+    Ok(Transfers { transfers })
+}
+
 
 #[substreams::handlers::map]
 pub fn graph_out(
