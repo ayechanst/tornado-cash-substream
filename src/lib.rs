24a3610e4bfa38e9bbf24d308e0c00fd747db215
Chad Bro <ayechan.sant@gmail.com>
2023-10-23 15:12:30 -0700

    should work

--- src/lib.rs
+++ src/lib.rs
@@ -1,46 +1,23 @@
 #[path = "./abi/TornadoRouter.rs"]
 mod erc721;
 mod helpers;
-mod pb;
+mod pb  } else {
+//                     None
+//                 }
+//             } else {
+//                 None
+//             }
+//         })
+//         .map(|(transfer, hash)| Transfer {
+//             from: format_hex(&transfer.from),
+//             to: format_hex(&transfer.to),
+//             token_id: transfer.token_id.to_string(),
+//             tx_hash: hash,
+//         })
+//         .collect::<Vec<Transfer>>();
 
-use pb::schema::{Transfer, Transfers, Deposit, Deposits};
-// use substreams::{pb::substreams::Clock, scalar::BigInt};
-// use substreams_entity_change::{pb::entity::EntityChanges, tables::Tables};
-use substreams_ethereum::pb::eth;
-
-use helpers::*;
-
-// use substreams::scalar::{BigDecimal, BigInt};
-use erc721::events::Transfer as TransferEvent;
-
-pub const ADDRESS: &str = "0xd90e2f925DA726b50C4Ed8D0Fb90Ad053324F31b";
-const START_BLOCK: u64 = 12287507;
-
-#[substreams::handlers::map]
-fn map_transfers(block: eth::v2::Block) -> Result<Transfers, substreams::errors::Error> {
-    let transfers = block
-        .logs()
-        .filter_map(|log| {
-            if format_hex(log.address()) == ADDRESS.to_lowercase() {
-                if let Some(transfer) = TransferEvent::match_and_decode(log) {
-                    Some((transfer, format_hex(&log.receipt.transaction.hash)))
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        })
-        .map(|(transfer, hash)| Transfer {
-            from: format_hex(&transfer.from),
-            to: format_hex(&transfer.to),
-            token_id: transfer.token_id.to_string(),
-            tx_hash: hash,
-        })
-        .collect::<Vec<Transfer>>();
-
-    Ok(Transfers { transfers })
-}
+//     Ok(Transfers { transfers })
+// }
 
 #[substreams::handlers::map]
 fn map_deposits(block: eth::v2::Block) -> Result<Deposits, substreams::errors::Error> {
@@ -49,22 +26,22 @@ fn map_deposits(block: eth::v2::Block) -> Result<Deposits, substreams::errors::E
         .filter_map(|callview| {
             if format_hex(&callview.call.address) == ADDRESS.to_lowercase() {
                 if let Some(value) = callview.call.value {
+                    println!("value: {}", value);
                     Some(Deposit {
-                        // use callview feilds
-                        from: format_hex(&value.from),
-                        to: format_hex(&transfer.to),
-                        tx_hash: hash,
-                        tx_value: deposit,
+                        from: format_hex(&callview.transaction.from),
+                        to: format_hex(&callview.transaction.to),
+                        tx_hash: format_hex(&callview.transaction.hash),
+                        tx_value: "big int".to_string(),
                     })
-                    .collect::<Vec<Deposits>>();
-                Ok(Deposits { deposits })
                 } else {
                     None
                 }
             } else {
                 None
             }
-        });
+        })
+            .collect::<Vec<Deposit>>();
+            Ok(Deposits { deposits })
         // .map(|(deposit, hash)| Deposits {
         //     from: format_hex(&transfer.from),
         //     to: format_hex(&transfer.to),
