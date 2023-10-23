2da59c10dd1869fc9becfcdb24df6ea97aadcca0
Chad Bro <ayechan.sant@gmail.com>
2023-10-23 12:50:17 -0700

    test map_transactions

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ build:
 
 .PHONY: stream
 stream: build
-	substreams run -e $(ENDPOINT) substreams.yaml map_block -s 12292922 -t +10
+	substreams run -e $(ENDPOINT) substreams.yaml map_transfers -s 12292922 -t +10
 
 .PHONY: protogen
 protogen:
--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,15 @@ mod erc721;
 mod helpers;
 mod pb;
 
-use pb::schema::{Transfer, Transfers, Deposit, Deposits, Withdraw, Withdraws};
-use substreams::{pb::substreams::Clock, scalar::BigInt};
+use pb::schema::{Transfer, Transfers, Deposits};
+// use substreams::{pb::substreams::Clock, scalar::BigInt};
 use substreams_entity_change::{pb::entity::EntityChanges, tables::Tables};
-use substreams_ethereum::{pb::eth, Event};
+use substreams_ethereum::pb::eth;
 
 use helpers::*;
 
-use substreams::scalar::{BigDecimal, BigInt};
-use erc721::events::{Transfer as TransferEvent, Deposit as DepositEvent};
+// use substreams::scalar::{BigDecimal, BigInt};
+use erc721::events::Transfer as TransferEvent;
 
 pub const ADDRESS: &str = "0xd90e2f925DA726b50C4Ed8D0Fb90Ad053324F31b";
 const START_BLOCK: u64 = 12287507;
@@ -42,32 +42,33 @@ fn map_transfers(block: eth::v2::Block) -> Result<Transfers, substreams::errors:
     Ok(Transfers { transfers })
 }
 
-#[substreams::handlers::map]
-fn map_deposits(block: eth::v2::Block) -> Result<Deposits, substreams::errors::Error> {
-    let deposits = block
-        .calls()
-        .filter_map(|tx| {
-            if format_hex(&tx.call.address) == ADDRESS.to_lowercase() {
-                if let Some(value) = tx.call.value {
-                    // adjust this to fit the case
-                    BigInt::from_signed_bytes_be(&storage_change.old_value);
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
-        })
-        .map(|(deposit, hash)| Deposits {
-            from: format_hex(&transfer.from),
-            to: format_hex(&transfer.to),
-            tx_hash: hash,
-            tx_value: deposit,
-        })
-        .collect::<Vec<Deposits>>();
+// #[substreams::handlers::map]
+// fn map_deposits(block: eth::v2::Block) -> Result<Deposits, substreams::errors::Error> {
+//     let deposits = block
+//         .calls()
+//         .filter_map(|tx| {
+//             if format_hex(&tx.call.address) == ADDRESS.to_lowercase() {
+//                 if let Some(value) = tx.call.value {
+//                     Some((value, tx.hash.clone()))
+//                 } else {
+//                     None
+//                 }
+//             } else {
+//                 None
+//             }
+//         })
+//         .map(|(deposit, hash)| Deposits {
+//             from: format_hex(&transfer.from),
+//             to: format_hex(&transfer.to),
+//             tx_hash: hash,
+//             tx_value: deposit,
+//         })
+//         .collect::<Vec<Deposits>>();
 
-    Ok(Deposits { deposits })
-}
+//     Ok(Deposits { deposits })
+// }
+
+
 
 #[substreams::handlers::map]
 pub fn graph_out(
