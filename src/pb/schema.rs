9de0cc78a056cdcf5ca0c175c97fa45c8e68b63b
Chad Bro <ayechan.sant@gmail.com>
2023-11-02 13:54:36 -0700

    trimmed pb

--- src/pb/schema.rs
+++ src/pb/schema.rs
@@ -28,25 +28,7 @@ pub struct Deposit {
     pub to: ::prost::alloc::string::String,
     #[prost(string, tag="3")]
     pub tx_hash: ::prost::alloc::string::String,
-    #[prost(uint64, tag="4")]
-    pub tx_value: u64,
-}
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct Withdraws {
-    #[prost(message, repeated, tag="1")]
-    pub withdraws: ::prost::alloc::vec::Vec<Withdraw>,
-}
-#[allow(clippy::derive_partial_eq_without_eq)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct Withdraw {
-    #[prost(string, tag="1")]
-    pub from: ::prost::alloc::string::String,
-    #[prost(string, tag="2")]
-    pub to: ::prost::alloc::string::String,
-    #[prost(string, tag="3")]
-    pub tx_hash: ::prost::alloc::string::String,
-    #[prost(uint64, tag="4")]
-    pub tx_value: u64,
+    #[prost(string, tag="4")]
+    pub tx_value: ::prost::alloc::string::String,
 }
 // @@protoc_insertion_point(module)
