d14b300fc3975d30df7ec8b908f3744729901e73
ayechan <ayechan.sant@gmail.com>
2024-03-19 11:38:22 -0700

    pool name macro

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,10 +1,12 @@
+use std::collections::HashMap;
+
 // use crate::{
 //     pb::schema::{Approvals, Transfers},
 //     pb::schema::Transfers,
 //     ADDRESS,
 // };
 //
-use substreams::{Hex, scalar::BigInt};
+use substreams::{scalar::BigInt, Hex};
 // use substreams_entity_change::tables::Tables;
 
 // pub fn transfers_to_table_changes(tables: &mut Tables, transfers: &Transfers) {
@@ -27,12 +29,27 @@ use substreams::{Hex, scalar::BigInt};
 //             .set("owner", &transfer.to);
 //     }
 // }
+pub const TC01: &str = "0x12D66f87A04A9E220743712cE6d9bB1B5616B8Fc";
+pub const TC1: &str = "0x47CE0C6eD5B0Ce3d3A51fdb1C52DC66a7c3c2936";
+pub const TC10: &str = "0x910Cbd523D972eb0a6f4cAe4618aD62622b39DbF";
+pub const TC100: &str = "0xA160cdAB225685dA1d56aa342Ad8841c3b53f291";
 
+pub fn wei_to_eth(wei: BigInt) -> f32 {
+    ((wei.to_u64()) / 1000000000000000000) as f32
+}
 
 pub fn format_hex(address: &[u8]) -> String {
     format!("0x{}", Hex(address).to_string())
 }
 
-pub fn wei_to_eth(wei: BigInt) -> f32 {
-    ((wei.to_u64()) / 1000000000000000000) as f32
+pub fn get_pool_name(address: &str) -> String {
+    let mut address_to_name = HashMap::new();
+    address_to_name.insert(TC01.to_lowercase(), "0.1 Eth Pool");
+    address_to_name.insert(TC1.to_lowercase(), "1 Eth Pool");
+    address_to_name.insert(TC10.to_lowercase(), "10 Eth Pool");
+    address_to_name.insert(TC100.to_lowercase(), "100 Eth Pool");
+    match address_to_name.get(address) {
+        Some(pool_name) => pool_name.to_string(),
+        None => "Unknown".to_string(),
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,10 @@ mod erc721;
 mod helpers;
 mod pb;
 
-// use std::vec;
-
 use helpers::*;
 use pb::schema::{Deposit, Deposits};
-// use substreams_ethereum::block_view::LogView;
-// use substreams_ethereum::block_view::ReceiptView;
-// use substreams_ethereum::pb::eth;
 use substreams_ethereum::pb::eth::v2::Block;
+
 // store related things
 use substreams::{
     log,
@@ -19,14 +15,14 @@ use substreams::{
 };
 
 // database related things
-use substreams_database_change::pb::database::DatabaseChanges;
-// use substreams_database_change::tables::Tables;
 
 pub const ADDRESS: &str = "0xd90e2f925DA726b50C4Ed8D0Fb90Ad053324F31b";
 pub const TC01: &str = "0x12D66f87A04A9E220743712cE6d9bB1B5616B8Fc";
 pub const TC1: &str = "0x47CE0C6eD5B0Ce3d3A51fdb1C52DC66a7c3c2936";
 pub const TC10: &str = "0x910Cbd523D972eb0a6f4cAe4618aD62622b39DbF";
 pub const TC100: &str = "0xA160cdAB225685dA1d56aa342Ad8841c3b53f291";
+pub const DEPOSIT_TOPIC_0: &str =
+    "0xa945e51eec50ab98c161376f0db4cf2aeba3ec92755fe2fcd388bdbbb80ff196";
 
 #[substreams::handlers::map]
 fn map_deposits(blk: Block) -> Result<Deposits, substreams::errors::Error> {
@@ -38,15 +34,20 @@ fn map_deposits(blk: Block) -> Result<Deposits, substreams::errors::Error> {
                 || format_hex(&log.log.address) == TC10.to_lowercase()
                 || format_hex(&log.log.address) == TC100.to_lowercase()
             {
-                let value = &log.receipt.transaction.value;
-                if let Some(value) = value {
-                    Some(Deposit {
-                        from: format_hex(&log.receipt.transaction.from),
-                        to: format_hex(&log.receipt.transaction.to),
-                        address: format_hex(&log.log.address),
-                        tx_hash: format_hex(&log.receipt.transaction.hash),
-                        tx_value: BigInt::from_unsigned_bytes_be(&value.bytes).to_string(),
-                    })
+                if format_hex(&log.log.topics[0]) == DEPOSIT_TOPIC_0.to_lowercase() {
+                    let address = format_hex(&log.log.address);
+                    let value = &log.receipt.transaction.value;
+                    if let Some(value) = value {
+                        Some(Deposit {
+                            from: format_hex(&log.receipt.transaction.from),
+                            to: format_hex(&log.receipt.transaction.to),
+                            address: get_pool_name(&address),
+                            tx_hash: format_hex(&log.receipt.transaction.hash),
+                            tx_value: BigInt::from_unsigned_bytes_be(&value.bytes).to_string(),
+                        })
+                    } else {
+                        None
+                    }
                 } else {
                     None
                 }
@@ -62,36 +63,12 @@ fn map_deposits(blk: Block) -> Result<Deposits, substreams::errors::Error> {
 fn store_deposits(deposits: Deposits, store: StoreAddBigInt) {
     for deposit in deposits.deposits {
         let value_as_string = deposit.tx_value;
+        substreams::log::info!("value as string {}", value_as_string);
         let value_as_bigint: BigInt = value_as_string.parse().unwrap();
+        substreams::log::info!("value as bigint {}", value_as_bigint);
         store.add(0, "total", value_as_bigint);
     }
 }
 
-#[substreams::handlers::map]
-pub fn db_out(
-    deposits: Deposits,
-    all_deposits: StoreGetBigInt,
-) -> Result<DatabaseChanges, substreams::errors::Error> {
-    let mut tables = substreams_database_change::tables::Tables::new();
-
-    for deposit in deposits.deposits {
-        let key = format!("{}:{}", deposit.from, deposit.tx_hash);
-        let deposit_as_eth = wei_to_eth(deposit.tx_value.parse().unwrap());
-        tables
-            .create_row("deposits", key)
-            .set("from_address", deposit.from)
-            .set("to_address", deposit.to)
-            .set("tx_hash", deposit.tx_hash)
-            .set("tx_value", deposit_as_eth.to_string());
-    }
-
-    if let Some(value) = all_deposits.get_at(0, "total") {
-        tables
-            .create_row("total_deposits", ADDRESS)
-            .set("total_value", value.to_string());
-    }
-
-    // transfers_to_database_changes(&mut tables, &transfers);
-
-    Ok(tables.to_database_changes())
-}
+// #[substreams::handlers::map]
+// fn graph_out(deposits: Deposits) -> Result<EntityChanges, substreams::errors::Error> {}
